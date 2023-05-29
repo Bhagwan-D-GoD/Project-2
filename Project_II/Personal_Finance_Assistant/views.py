b8eaca04eb076206a9666fb102573a7c8bbbdcc0
Bhagwan <sandipdevkota9@gmail.com>
2023-05-29 17:20:21 +0545

    User creation and athunetication completed

--- Project_II/Personal_Finance_Assistant/views.py
+++ Project_II/Personal_Finance_Assistant/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import messages
 from django.shortcuts import render,redirect
+from django.contrib.auth.hashers import make_password,check_password
+from django.contrib.auth import authenticate 
+from.models import User_Info
 # Create your views here.
 
 def index(request):
@@ -11,30 +14,39 @@ def register(request):
     if request.method == 'POST':
         try:
             name = request.POST.get("name")
-            user_id = request.POST.get("user_id")
+            u_id = request.POST.get("user_id")
             email = request.POST.get("email")
             password = request.POST.get("password")
             confirm_password = request.POST.get("confirm_password")
+            if User_Info.objects.filter(user_id=u_id).exists():
+                messages.error(request,"user already exists")
+                return redirect('index') 
             if password != confirm_password:
                 print("Password does not match")
                 messages.error(request,"Password does not match")
                 return(redirect('index'))
-            #to check if the user_id already exists or not 
-            #if User_Info.objects.filter(user_id=u_id).exists():
-                #User.objects.filter(username=self.cleaned_data['username']).exists():
-                #messages.error(request,"user already exists")
-                #return render(request, 'login.html') 
-         
-                #messages.success(request, 'Account created succesfully. Now you can login.')
-                #return render(request, 'login.html')
+            password = make_password(password) #to encrpt the password 
+            newaccount = User_Info()
+            newaccount.user_name = name
+            newaccount.user_id = u_id
+            newaccount.password = password
+            newaccount.email = email
+            newaccount.save()
+            messages.success(request,"Account created sucessfully!")
         except IndexError:
-            #This means that the face_recognition module couldn't find any faces i
-        
-            messages.error(request,"face is not detected in the provided image please provide correct image")
             return redirect("index")
     return(render(request,"index.html"))
 
 def login(request):
-    if request.method =="POST":
-        messages.error(request,"Password does not match")
-        return(redirect('index'))
+    try:
+        if request.method =="POST":
+            u_id = request.POST.get("user_id")
+            pass_word = request.POST.get("password")
+            if User_Info.objects.filter(user_id=u_id).exists():
+                user = User_Info.objects.get(user_id=u_id)
+                if check_password(pass_word,user.password):#compares plain password and hashed password
+                    return(HttpResponse('welcome to our site'))
+            messages.error(request,"Invalid Username or Password")
+            return(redirect('index'))
+    except:
+        return redirect(index)
