14e4b65a2fc630c33880e6a211401f44fc745fe3
Michael ale <michaelale200@gmail.com>
2023-10-02 12:10:15 +0545

    added new app STOCKS

--- Project_II/Personal_Finance_Assistant/models.py
+++ Project_II/Personal_Finance_Assistant/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User_Info(models.Model):
     user_id = models.CharField(primary_key=True,max_length=10,)
     user_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
+    email = models.EmailField(max_length=60)
     password = models.CharField(max_length=10000)
     
     def __str__(self):
