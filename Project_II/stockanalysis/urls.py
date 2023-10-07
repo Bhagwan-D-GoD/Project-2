875bb35260b0f7730b179d654fb5aa0d125bc8fe
Michael ale <michaelale200@gmail.com>
2023-10-07 13:57:27 +0545

    added streamlit

--- Project_II/stockanalysis/urls.py
+++ Project_II/stockanalysis/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     #path('stockchart/', views.stock_chart, name='stockchart'),
     path('stockchart/', views.stock_chart_real, name='stockchart'),
     path('filterstocks/', views.filterstocks, name='filterstocks'),
+    path('streamlit/', views.streamlit_page, name='streamlit_page'),
     #path('analyst_recommendations/', views.stock_analyst_recommendations, name='stock_analyst_recommendations'),
+    
 ]
